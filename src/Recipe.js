d388e25cf9f777d49a3f2d896b12d418a57ee3a1
Lo <18424232+Lokode@users.noreply.github.com>
2020-10-23 23:39:50 -0400

    working recipe api

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,9 +1,15 @@
 import React from "react";
+import style from "./recipe.module.css";
 
-const Recipe = ({ title, Calories, image }) => {
+const Recipe = ({ title, Calories, image, ingredients }) => {
   return (
-    <div>
+    <div className={style.recipe}>
       <h1> {title} </h1>
+      <ol>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ol>
       <p> {Calories}</p>
       <img src={image} alt="" />
     </div>
